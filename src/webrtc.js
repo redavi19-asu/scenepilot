2ffcc274c09958ef36cd31e891904ef8e8e91238
Ryan E Davis <redavi19@asu.edu>
2026-09-06 15:26:12 -0400

    Fix Safari remote media stream handling in ScenePilot

--- src/webrtc.js
+++ src/webrtc.js
@@ -6,10 +6,15 @@ export function createPeerConnection({
   const peer = new RTCPeerConnection({
     iceServers: [
       {
-        urls: "stun:stun.l.google.com:19302"
+        urls: [
+          "stun:stun.l.google.com:19302",
+          "stun:stun1.l.google.com:19302"
+        ]
       }
     ]
   });
+
+  const fallbackStream = new MediaStream();
 
   peer.onicecandidate = event => {
     if (event.candidate && onIceCandidate) {
@@ -18,10 +23,18 @@ export function createPeerConnection({
   };
 
   peer.ontrack = event => {
-    const incomingStream = event.streams?.[0];
+    let incomingStream = event.streams?.[0];
 
-    if (incomingStream && onTrack) {
-      onTrack(incomingStream);
+    if (!incomingStream) {
+      if (!fallbackStream.getTracks().some(track => track.id === event.track.id)) {
+        fallbackStream.addTrack(event.track);
+      }
+
+      incomingStream = fallbackStream;
+    }
+
+    if (onTrack) {
+      onTrack(incomingStream, event.track);
     }
   };
 
