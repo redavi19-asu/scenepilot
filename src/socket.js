07d0d8dd5847e32d7fe300b70170290d4c51fa0a
Ryan E Davis <redavi19@asu.edu>
2026-09-06 12:04:34 -0400

    Restore proven ScenePilot Codespaces signaling URL

--- src/socket.js
+++ src/socket.js
@@ -13,7 +13,7 @@ function getSignalUrl() {
   ) {
     const signalHost =
       hostname.replace(
-        /-\d+\.app\.github\.dev$/,
+        /-5173\.app\.github\.dev$/,
         "-3001.app.github.dev"
       );
 
