cc27105a3e202e3e7866fcdfa2535c1fa82ceaae
Ryan E Davis <redavi19@asu.edu>
2026-09-07 10:57:34 -0400

    Route ScenePilot through SaaS portal

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
+import ScenePilotPortal from "./ScenePilotPortal.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <App />
+  <ScenePilotPortal />
 );
-
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
